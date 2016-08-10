688d24c859d2ac66d17a9a5e68415fad3ee2d4bc
falkTX <falktx@gmail.com>
2016-08-10 14:25:55 +0200

    Update fat1.lv2 bundle

--- plugins-fixed/fat1.lv2/modgui/x42-autotune.js
+++ plugins-fixed/fat1.lv2/modgui/x42-autotune.js
@@ -1,16 +1,63 @@
 function (event) {
 
+	/* mask midi selected notes */
+	function mask_key (id, midimode) {
+		var sym = id;
+		if (id < 10) { sym = "0" + id; }
+		var key = event.icon.find ('[mod-port-symbol=m' + sym +']');
+		if (midimode > 0) {
+			key.removeClass ('midioff');
+			key.addClass ('midion');
+		} else if (midimode < 0) {
+			key.removeClass ('midion');
+			key.addClass ('midioff');
+		} else {
+			/* note is under user control */
+			key.removeClass ('midion');
+			key.removeClass ('midioff');
+		}
+	}
+
+	/* highlight detected note(s) */
+	function set_key (id, on) {
+		var sym = id;
+		if (id < 10) { sym = "0" + id; }
+		var key = event.icon.find ('[mod-port-symbol=m' + sym +']');
+		if (on) {
+			key.addClass ('active');
+		} else {
+			key.removeClass ('active');
+		}
+	}
+
 	/* top-level entry, called from mod-ui */
 	if (event.type == 'start') {
-		var pk = event.icon.find ('[mod-role=piano-keyboard]');
-
 		var ports = event.ports;
 		for (var p in ports) {
-			// ports[p].symbol, ports[p].value;
+			if (ports[p].symbol == 'mode') {
+				var pk = event.icon.find ('[mod-role=piano-keyboard]');
+				if (event.value == 1) {
+					pk.css('display', 'none');
+				} else {
+					pk.css('display', 'block');
+				}
+			}
 		}
 	}
 	else if (event.type == 'change') {
-		var pk = event.icon.find ('[mod-role=piano-keyboard]');
-		// event.symbol, event.value
+		if (event.symbol == 'mode') {
+			var pk = event.icon.find ('[mod-role=piano-keyboard]');
+			if (event.value == 1) {
+				pk.css('display', 'none');
+			} else {
+				pk.css('display', 'block');
+			}
+			// prepare for output params -- MIDI set keys
+			if (event.value == 2) {
+				for (k = 0; k < 12; ++k) {
+					mask_key (k, 0);
+				}
+			}
+		}
 	}
 }
