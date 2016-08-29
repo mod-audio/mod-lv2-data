2ace9bce81cab03e7ed193f9dcb4c45f0386f463
falkTX <falktx@gmail.com>
2016-08-29 10:27:45 +0200

    Update fat1.lv2 bundle

--- plugins-fixed/fat1.lv2/modgui/x42-autotune.js
+++ plugins-fixed/fat1.lv2/modgui/x42-autotune.js
@@ -30,34 +30,83 @@ function (event) {
 		}
 	}
 
+	function update_midi_mask (data) {
+		if (data.mode == 2 || (data.mode == 0 && data.nmask == 0)) {
+			/* manual mode -- control input applies */
+			/* auto mode -- control applies IFF no midi notes are set */
+			for (k = 0; k < 12; ++k) {
+				mask_key (k, 0);
+			}
+			return;
+		}
+		/* midi rulez */
+		for (k = 0; k < 12; ++k) {
+			mask_key (k, (Math.floor (data.nmask) & (1 << k)) == 0 ? -1 : 1);
+		}
+	}
+
+	function update_note_set (data) {
+		for (k = 0; k < 12; ++k) {
+			set_key (k, Math.floor (data.nset) & (1 << k));
+		}
+	}
+
+	function update_pitcherror (err) {
+		var bar = event.icon.find ('[mod-role=pitch-error]');
+		if (Math.abs (err) > 0.33) {
+			bar.addClass ('bad');
+		} else {
+			bar.removeClass ('bad');
+		}
+		var pos = 63 + 64 * err;
+		bar.css ('left', pos + 'px'); /* CSS or style? XXX */
+	}
+
 	/* top-level entry, called from mod-ui */
 	if (event.type == 'start') {
 		var ports = event.ports;
+		var data = {};
+		data.mode = 0;
+		data.nmask = 0;
+		data.nset = 0;
 		for (var p in ports) {
-			if (ports[p].symbol == 'mode') {
-				var pk = event.icon.find ('[mod-role=piano-keyboard]');
-				if (event.value == 1) {
-					pk.css('display', 'none');
-				} else {
-					pk.css('display', 'block');
-				}
+			if (event.symbol == 'error') {
+				update_pitcherror (event.value);
+			}
+			else if (ports[p].symbol == 'mode') {
+				data.mode = event.value;
+			}
+			else if (ports[p].symbol == 'nmask') {
+				data.nmask = event.value;
+			}
+			else if (ports[p].symbol == 'nset') {
+				data.nset = event.value;
 			}
 		}
+		update_midi_mask (data);
+		update_note_set (data);
+		var ds = event.icon.find ('[mod-role=drag-handle]');
+		ds.data ('persist', data);
 	}
 	else if (event.type == 'change') {
-		if (event.symbol == 'mode') {
-			var pk = event.icon.find ('[mod-role=piano-keyboard]');
-			if (event.value == 1) {
-				pk.css('display', 'none');
-			} else {
-				pk.css('display', 'block');
-			}
-			// prepare for output params -- MIDI set keys
-			if (event.value == 2) {
-				for (k = 0; k < 12; ++k) {
-					mask_key (k, 0);
-				}
-			}
+		var ds = event.icon.find ('[mod-role=drag-handle]');
+		var data = ds.data ('persist');
+		if (event.symbol == 'error') {
+			update_pitcherror (event.value);
+			return;
 		}
+		else if (event.symbol == 'mode') {
+			data.mode = event.value;
+			update_midi_mask (data);
+		}
+		else if (event.symbol == 'nmask') {
+			data.nmask = event.value;
+			update_midi_mask (data);
+		}
+		else if (event.symbol == 'nset') {
+			data.nset = event.value;
+			update_note_set (data);
+		}
+		ds.data ('persist', data);
 	}
 }
