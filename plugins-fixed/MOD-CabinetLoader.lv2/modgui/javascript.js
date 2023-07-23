84af926217cf80fdfadc2cf9a7fd88fe77a2ba38
falkTX <falktx@falktx.com>
2023-07-23 13:09:27 +0200

    Add modgui for cabinet loader
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins-fixed/MOD-CabinetLoader.lv2/modgui/javascript.js
@@ -0,0 +1,32 @@
+function (event, funcs)
+{
+    function update_filename(icon, filename)
+    {
+        var index = filename.indexOf("/Speaker Cabinets IRs/");
+        if (index !== -1)
+            filename = filename.slice(index+22);
+
+        index = filename.indexOf("/MOD-CabinetLoader.lv2/");
+        if (index !== -1)
+            filename = filename.slice(index+23);
+
+        icon.find('.file-info-details').text(filename);
+    }
+
+    if (event.type == 'start')
+    {
+        for (var i in event.parameters)
+        {
+            if (event.parameters[i].uri === 'https://mod.audio/plugins/CabinetLoader#irfile')
+            {
+                update_filename(event.icon, event.parameters[i].value);
+                break;
+            }
+        }
+    }
+    else if (event.type == 'change')
+    {
+        if (event.uri === 'https://mod.audio/plugins/CabinetLoader#irfile')
+            update_filename(event.icon, event.value);
+    }
+}
